e5d2ecf9cbbec298378995485d0d798aa05989b8
Marcos0098 <115181147+Marcos0098@users.noreply.github.com>
2022-11-16 16:58:43 -0300

    Update game.jsx
    realizado um update no arquivo adicionando informações como Tags, Game Description, Developers.
    
    foi criado objeto JSON para juntar os dados de outros dois objetos JSON para a criação dos links que levam as lojas.

--- encontra-jogos/src/Pages/Game.jsx
+++ encontra-jogos/src/Pages/Game.jsx
@@ -1,8 +1,6 @@
 import { useEffect, useState } from "react";
 import { useParams} from "react-router-dom";
 
-import { FaStar } from "react-icons/fa"
-
 import GameCard from "../components/GameCard";
 
 import "./game.css";
@@ -16,31 +14,50 @@ const Game = () => {
   const [screenshot, setScreenshot] = useState(null)
   const [lojas, setLojas] = useState(null);
 
-  const getGame = async (url, urlScreenshot, urlLojas, storesUrl) =>{
+  const idUrlStore = {
+    idLoja : [],
+    urlLoja : [],
+    nomeLoja : [],
+  }
+
+  if(game != null){
+    for(let i = 0 ; i < game.stores.length; i++){
+      idUrlStore.idLoja[i] = lojas[i].store_id;
+      idUrlStore.urlLoja[i] = lojas[i].url;
+      idUrlStore.nomeLoja[i] = game.stores[i].store.name;
+    }
+  }
+
+  const getGame = async (url, urlScreenshot, urlLojas) =>{
 
     const resGame = await fetch(url);
     const resScreenshot = await fetch(urlScreenshot);
     const resLojas = await fetch(urlLojas);
-    const resStores = await fetch(storesUrl);
+
+
     const dataGame = await resGame.json();
     const dataScreen = await resScreenshot.json();
     const dataLojas = await resLojas.json();
-    const dataStores = await resStores.json();
+
 
     setGame(dataGame);
     setScreenshot(dataScreen);
     setLojas(dataLojas.results)
-    console.log(dataStores.results)
+
     console.log(dataGame)
   }
+
+
+
+
   useEffect(()=>{
     const gameUrl = `${gameURL}/${id}?${apiKey}`;
     const screenshotUrl = `${gameURL}/${id}/screenshots?${apiKey}`;
     const lojasOficiaisUrl = `${gameURL}/${id}/stores?${apiKey}`
-    const storesUrl = `https://api.rawg.io/api/stores?${apiKey}`;
-    getGame(gameUrl, screenshotUrl, lojasOficiaisUrl, storesUrl);
+    getGame(gameUrl, screenshotUrl, lojasOficiaisUrl);
 
   },[])
+
   return (
     <div className="game-description">
       {game &&
@@ -49,22 +66,39 @@ const Game = () => {
         <h3 className="info-genero">
           Generos:{game.genres.length > 0 && game.genres.map((genero) => <div key={genero.id} className="generos">{genero.name}</div>)}
         </h3>
+
         <h3 className="info-plataforma">
           Plataformas: {game.platforms.length > 0 &&
           game.platforms.map((plataforma) => 
           <div key={plataforma.platform.id}  className="plataformas">{plataforma.platform.name}</div>)}
         </h3>
+
         <h3 className="info-screenshot">
           Screenshots: {screenshot.results.length > 0 &&
           screenshot.results.map((imagens) => 
           <div key={imagens.id} className="screenshots" ><img src={imagens.image}/></div>)}
         </h3>
+
         <h3 className="info-store">
           Lojas: {game.stores.length > 0 && 
-          game.stores.map((sites) =>
-          <div key={sites.id} className="lojas"><a href="" target="_blank">{sites.store.name}</a></div>)} 
+          idUrlStore.idLoja.map((idSites, index) => 
+          <div key={idSites} className="Link-lojas"> <a href={idUrlStore.urlLoja[index]} target="_blank">{idUrlStore.nomeLoja[index]}</a> </div>
+          )}
         </h3>
-        <p>TODO name-done, genero-done, platform-done, shot_screenshots-done, store, tags,background-image, metacritic</p>
+        
+        <h3 className="info-tags">
+          Tags: {game.tags.length > 0 &&
+          game.tags.map((categorias) => <div>{categorias.name}</div>)}
+        </h3>
+
+        <h3 className="info-description">
+          Description: {game.description_raw}
+        </h3>
+
+        <h3 className="info-developers">
+          Developers: {game.developers.length > 0 &&
+          game.developers.map((devs) => <div key={devs.id} className="devs">{devs.name}</div>)}
+        </h3>
       </>
       }
 
